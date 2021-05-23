6544aecc2ec5ab98d7ac538af77657d5c8ab24be
kokage <k0kag3.k0kag3+github@gmail.com>
2021-05-23 17:58:31 +0900

    chore: update example

--- examples/iterateOverUpdates.js
+++ examples/iterateOverUpdates.js
@@ -1,7 +1,7 @@
 const { iterateLatestUpdates } = require("..");
 
 async function main() {
-  for await (const updates of iterateLatestUpdates({ type: "Writing" })) {
+  for await (const { updates } of iterateLatestUpdates({ type: "Writing" })) {
     for (const update of updates) {
       console.log(update.title);
     }
