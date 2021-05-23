b4eab53581ee2dc992b220ca996eb2e4ed41f7f5
kokage <k0kag3.k0kag3+github@gmail.com>
2021-05-23 15:21:51 +0900

    fix: export interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
-export { getLatestUpdates, iterateLatestUpdates, SearchType } from "./latest";
-export { getItemDetail } from "./item";
+export {
+  getLatestUpdates,
+  iterateLatestUpdates,
+  SearchType,
+  Update,
+  PageCursor,
+  GetLatestUpdatesResponse,
+  GetLatestUpdatesOptions,
+} from "./latest";
+export {
+  getItemDetail,
+  Item,
+  ImageItem,
+  WritingItem,
+  OtherMaterialItem,
+  Tag,
+} from "./item";
 export { verifyUser } from "./user";
 export { AryionUserNotFoundError } from "./error";
