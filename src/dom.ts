ebbbeb99fa460b438146230ceed970518ef779e3
kokage <k0kag3.k0kag3+github@gmail.com>
2021-05-24 09:19:54 +0900

    fix: throw error when retry count exceeded

--- src/dom.ts
+++ src/dom.ts
@@ -1,20 +1,26 @@
 import fetch from "node-fetch";
 import { JSDOM } from "jsdom";
 
-export async function getDOM(endpoint: string): Promise<Document | undefined> {
+export async function getDOM(endpoint: string): Promise<Document> {
   let retryCount = 3;
-  while (retryCount > 0) {
+  let res: string;
+  while (true) {
     try {
-      const {
-        window: { document },
-      } = new JSDOM(await fetch(endpoint).then((res) => res.text()));
-      return document;
+      res = await fetch(endpoint).then((res) => res.text());
+      break;
     } catch (err) {
       if (err.code === "ETIMEOUT") {
+        if (retryCount <= 0) {
+          throw err;
+        }
         retryCount -= 1;
       } else {
         throw err;
       }
     }
   }
+  const {
+    window: { document },
+  } = new JSDOM(res);
+  return document;
 }
